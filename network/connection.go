5d3521ec3fd5dd14fd05a56f3f190292e5dc7a5c
Laykel <layke.luc@gmail.com>
2019-11-20 16:43:46 +0100

    Start of protocol definition

---
+++ network/connection.go
@@ -0,0 +1,9 @@
+/*
+Lab 2 - mutual exclusion
+File: network/connection.go
+Authors: Jael Dubey, Luc Wachter
+Go version: 1.13.4 (linux/amd64)
+
+Handle TCP connections, forward requests from the mutex controller
+*/
+package network
--- network/protocol.go
+++ network/protocol.go
@@ -4,8 +4,16 @@ File: network/protocol.go
 Authors: Jael Dubey, Luc Wachter
 Go version: 1.13.4 (linux/amd64)
 
+Describe networking values, messages structure for the protocol and provide
+encoding and decoding functions for messages
 */
 package network
+
+import (
+	"bytes"
+	"encoding/binary"
+	"log"
+)
 
 // Networking values
 // TODO: Maybe parameterize this through json?
@@ -16,14 +24,28 @@ const (
     End = 2
 )
 
+// Message to request the critical section
 type RequestCS struct {
-    hi uint32
-    i uint32
+	timestamp uint32
+	process   uint32
 }
 
+// Message to release the critical section
 type ReleaseCS struct {
-    hi uint32
-    i uint32
-    value int32
+	timestamp uint32
+	process   uint32
+	value     int32
+}
+
+// Encode given struct as big endian bytes and return bytes buffer
+func encode(message interface{}) *bytes.Buffer {
+	buffer := &bytes.Buffer{}
+	// Write struct's data as bytes
+	err := binary.Write(buffer, binary.BigEndian, message)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return buffer
 }
 
