14c71c47a294f3ab932c96a9c7e096b0b28d9a04
Jael24 <jael.dubey@heig-vd.ch>
2019-11-27 14:19:13 +0100

    Add parameters

--- network/protocol.go
+++ network/protocol.go
@@ -10,29 +10,26 @@ encoding and decoding functions for messages
 package network
 
 import (
-    "bytes"
-    "encoding/binary"
-    "log"
+	"bytes"
+	"encoding/binary"
+	"log"
 )
 
 // Networking values
-// TODO: Maybe parameterize this through json?
-const (
-    Port = 9706
-    Demand = 0
-    Wait = 1
-    End = 2
-)
+type Parameters struct {
+	InitialPort uint16 `json:"initial_port"`
+	NbProcesses uint8  `json:"nb_processes"`
+}
 
 // Message to request the critical section
 type RequestCS struct {
-    ProcessNbr uint32
+	ProcessNbr uint32
 	Timestamp  uint32
 }
 
 // Message to release the critical section
 type ReleaseCS struct {
-    ProcessNbr uint32
+	ProcessNbr uint32
 	Timestamp  uint32
 	Value      int32
 }
@@ -62,11 +59,11 @@ func DecodeRequest(buffer []byte) RequestCS {
 
 // Decode bytes from ReleaseCS back to struct
 func DecodeRelease(buffer []byte) ReleaseCS {
-    message := ReleaseCS{}
-    err := binary.Read(bytes.NewReader(buffer), binary.BigEndian, &message)
-    if err != nil {
-        log.Fatal(err)
-    }
+	message := ReleaseCS{}
+	err := binary.Read(bytes.NewReader(buffer), binary.BigEndian, &message)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    return message
+	return message
 }
